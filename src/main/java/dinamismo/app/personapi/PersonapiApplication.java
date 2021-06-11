3adb8d8ddf3d60fb680a845a7a4782fe1e1a1abf
jefersonlpsilva <jefersonsilvadev@hotmail.com>
2021-06-11 18:57:49 -0300

    commit inicial

---
+++ src/main/java/dinamismo/app/personapi/PersonapiApplication.java
@@ -0,0 +1,14 @@
+package dinamismo.app.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
