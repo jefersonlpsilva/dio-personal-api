7951e91936cce2adadcf5faf40cc9467f1535ae0
jefersonlpsilva <jefersonsilvadev@hotmail.com>
2021-06-14 00:50:47 -0300

    Developed update by id feature

--- src/main/java/dinamismo/app/personapi/service/PersonService.java
+++ src/main/java/dinamismo/app/personapi/service/PersonService.java
@@ -29,10 +29,7 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave  = personMapper.toModel(personDTO);
         Person savedPerson  = personRepository.save(personToSave);
-        return MessageResponseDTO.
-                builder().
-                message("Created person with ID "+savedPerson.getId()).
-                build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -51,6 +48,15 @@ public class PersonService {
         verifyIfExists(id);
         personRepository.deleteById(id);
     }
+    
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+        
+        Person personToUpdate  = personMapper.toModel(personDTO);
+        
+        Person updatePerson  = personRepository.save(personToUpdate);
+        return createMessageResponse(updatePerson.getId(), "Update person with ID ");
+    }
 
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository
@@ -58,4 +64,10 @@ public class PersonService {
                 .orElseThrow(() -> new PersonNotFoundException(id));
     }
     
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO.
+                builder().
+                message(message + id).
+                build();
+    }
 }
